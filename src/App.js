a63acf579b9c07e12af07015cc16890628597ca1
Epoggi <11327084+Epoggi@users.noreply.github.com>
2020-05-22 14:17:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 function App() {
+  const [pizzalist, setPizzalist] = useState([]);
+
+  useEffect(() => fetchData(), []);
+  const fetchData = () => {
+
+    fetch("https://harjoitusprojekti.herokuapp.com/restpizzat")
+      .then(response => response.json())
+      .then(data => setPizzalist(data))
+      console.log(pizzalist)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          pizzalist.map((pizzalist, index) =>
+            <Table style={{ width: 600 }}>
+              <TableBody>
+                <TableRow key={index}>
+                  <TableCell>
+                    {pizzalist.nimi}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          )
+        }
       </header>
     </div>
   );
